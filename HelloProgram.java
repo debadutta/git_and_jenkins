8d3f40bbff1dc6876d255f991f511e23dfa4b66d
DebaduttaOracle <debadutta.singh@oracle.com>
2020-10-14 16:01:30 +0530

    First Commit

---
+++ HelloProgram.java
@@ -0,0 +1,10 @@
+ public class HelloProgram{
+     public static void main(String[] args){
+            System.out.println("Loop based execution");
+
+            for(int i=0;i<10;i++){
+                    System.out.println(i);
+            }
+
+     }
+ }
